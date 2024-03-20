47bf89cb6b196433d03bd2ef764cf2e1ceb5b34d
root <lseirina87@gmail.com>
2024-03-20 22:42:39 +0300

    Added admin customization + testlist

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+"""
+Django admin customazation
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for user."""
+    ordering = ['id']
+    list_display = ['username']
+
+if  admin.site.is_registered(User):
+    admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""
+Tests for admin customization
+"""
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+
+class AdminTests(TestCase):
+    """Tests for admin."""
+    def  setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = User.objects.create_superuser(
+            username='AdminName',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = User.objects.create_user(
+            username='TestName',
+            password='testpass123',
+        )
+
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:auth_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.username)
